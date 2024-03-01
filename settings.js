40b5daac7257e03eed5ff2b8fc0b271cf234b9e8
bashri14 <156458311+bashri14@users.noreply.github.com>
2024-03-01 15:57:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,10 +29,10 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "BASHRIII-MD"
+global.ownernumber = '923358536644'
+global.ownername = 'MUBASHIR RAJPUT'
+global.ownerNumber = ["923358536644@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
@@ -40,9 +40,9 @@ global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.author = "BASHRI-MD"
+global.creator = "923358536644@s.whatsapp.net"
+global.prefa = [','.']
 global.sessionName = 'session'
 global.hituet = 0
 
